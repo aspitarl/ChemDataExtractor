cb8f499eaa3d864428c94175681351d5ac205232
Federico Madden <maddenfederico@gmail.com>
2020-08-13 12:29:53 -0700

    Update mc.py
    Fix measured concentration parser so as to function properly. Currently handles trivial constructions of cem + one of four stopwords+ concentration and vice versa

--- chemdataextractor/parse/mc.py
+++ chemdataextractor/parse/mc.py
@@ -15,7 +15,8 @@ import logging
 import re
 
 from chemdataextractor.parse.cem import cem
-from ..utils import first
+from chemdataextractor.parse.common import slash
+from chemdataextractor.utils import first
 from ..model import Compound, MeasuredConcentration
 from .actions import merge
 from .base import BaseParser
@@ -24,21 +25,22 @@ from .elements import W, R, Optional
 log = logging.getLogger(__name__)
 
 
-si_prefixes = R('[YZEPTGMkhcm\u03BCunpfzyad]') | R('da')
+si_prefixes_optional = '[YZEPTGMkhcm\u03BCunpfzyad]?'
 values = (R('[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?'))('value')#Matches floats + scientific notation
 
-molar_unit = Optional(si_prefixes) + R('M')
-mass_unit = Optional(si_prefixes) + R('(mol|g)')#I know that moles are a unit of quantity, don't @ me
-volume_unit = Optional(si_prefixes) + R('L')
-units = (molar_unit | (mass_unit + R('/') + Optional(volume_unit)))('units').add_action(merge)
+molar_unit =  R('[YZEPTGMkhcm\u03BCunpfzyad]?M')
+mass_unit =  R('[YZEPTGMkhcm\u03BCunpfzyad]?(mol|g)')#I know that moles are a unit of quantity, don't @ me
+volume_unit =  R('[YZEPTGMkhcm\u03BCunpfzya]?L')
+units = (molar_unit | mass_unit + slash + Optional(volume_unit))('units').add_action(merge)
 
+mc = (values + units)('mc')
 # TODO: figure out how to handle creatinine
 stopwords = W('was') | W('of') | W('concentration') | W('concentrations')
 left_bracket_parentheses = R('[[(]')
 right_bracket_parentheses = R('[)\]]')
 
-mc_phrase_cem_first = cem + Optional(stopwords).hide() + Optional(left_bracket_parentheses).hide() + units
-mc_phrase_cem_last = Optional(right_bracket_parentheses).hide() + units + Optional(stopwords).hide() + cem
+mc_phrase_cem_first = (cem + Optional(stopwords).hide() + Optional(left_bracket_parentheses).hide() + mc)('mc_phrase')
+mc_phrase_cem_last = (mc + Optional(stopwords).hide() + cem)('mc_phrase')
 
 mc_phrase = mc_phrase_cem_first | mc_phrase_cem_last
 
@@ -50,8 +52,8 @@ class McParser(BaseParser):
         compound = Compound(
             measured_concentrations=[
                 MeasuredConcentration(
-                    value=first(result.xpath('./mp/value/text()')),
-                    units=first(result.xpath('./mp/units/text()'))
+                    value=first(result.xpath('./mc/value/text()')),
+                    units=first(result.xpath('./mc/units/text()'))
                 )
             ]
         )
